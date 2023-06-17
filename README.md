12bb84f03b4e94cc4b12b9e2cda19a57b81c51db
Yassien Waleed <yassienwyh0@gmail.com>
2023-06-17 11:30:30 +0300

    updates

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
+# Deci Term 2 Project
+## Dynamic Landing Page Project
+
+Project Is About Dynamic Landing Page That Contain Comments Section To Give Your Opinion About It
+This Project Uses `html` `css` `javascript`
+List Of Skills Needed In Javascript 
+1. HTML
+2. CSS
+   - Meida Query
+3. JavaScript
+   - DOM
+       - Events
+          - LocalStorage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <div id="title" align="center">
   <h1>Deci Project Second Term</h1>
 </div>
--- js/app.js
+++ js/app.js
@@ -1,7 +1,37 @@
+const nav = document.querySelector('.nav-bar');
+const navWrapper = document.querySelector('.nav-bar .wrapper');
+const barsParent = document.createElement('li')
+const barOne = document.createElement('div')
+const barTwo = document.createElement('div')
+const barThree = document.createElement('div')
+const navUl = document.querySelector('.nav-bar .wrapper ul')
+
+
+barOne.classList.add('bar1')
+barTwo.classList.add('bar2')
+barThree.classList.add('bar3')
+barsParent.classList.add('bars')
+
+
+for (let i = 1; i <= 6; i++) {
+    let sectionLi = document.createElement('li');
+    sectionLi.setAttribute(`data-link-num`, i)
+    sectionLi.innerHTML = `Section ${i}`
+    if(i == 6) {
+        sectionLi.innerHTML = `Comments`
+    }
+    navUl.append(sectionLi)
+}
+
+barsParent.append(barOne)
+barsParent.append(barTwo)
+barsParent.append(barThree)
+navWrapper.append(barsParent)
+
+
 const section = document.querySelectorAll('.section');
 const overlay = document.querySelectorAll('.overlay')
 const circle = document.querySelectorAll('.circle')
-
 overlay.forEach(lay => {
     if(lay.dataset.dir == 'rtl') {
         lay.style.right = '10rem';
@@ -11,10 +41,8 @@ overlay.forEach(lay => {
         lay.style.transform =  'translate(0px, -3rem)';
     }
 });
-
 const links = document.querySelectorAll('ul li')
 const sections = document.querySelectorAll('[data-lin="true"]')
-
 for (let i = 0; i < sections.length; i++) {
     links[i].addEventListener('click', (e) => {
         document.querySelector('.bars').classList.remove('open');
@@ -33,7 +61,62 @@ for (let i = 0; i < sections.length; i++) {
         });
     })
 }
+function cmntBuilder(user,email,pic,cmnt,date) {
+    const box = document.createElement('div')
+    box.classList.add('box')
+    const img = document.createElement('img');
+    if(picture.value != '') {
+        img.src = `${picture.value}`;
+    }else {
+        img.src = pic;
+    }
+    const column = document.createElement('div')
+    column.classList.add('column')
+    cmntVal = document.createElement('p');
+    cmntVal.innerHTML =  cmnt;
+    cmntVal.id = `comment_${cmnt}`
+    cmntVal.classList.add('comment_val')
+    const emailVal = document.createElement('p');
+    emailVal.innerHTML =  email;
+    emailVal.classList.add('email_val')
+    const nameVal = document.createElement('p');
+    nameVal.innerHTML =  user;
+    nameVal.classList.add('name_val')
+    column.appendChild(nameVal)
+    column.appendChild(emailVal)
+    column.appendChild(cmntVal)
+    box.appendChild(img)
+    box.appendChild(column)
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const dateData = document.createElement('p')
+    dateData.classList.add('date')
+    dateData.innerText = date;
+    column.appendChild(dateData)
+    cmntSec.append(box)            
 
+    const optionsDiv = document.createElement('div')
+    optionsDiv.classList.add('options')
+    const toggle = document.createElement('div')
+    toggle.classList.add('toggle')
+    toggle.innerHTML = '...'
+    const opt = document.createElement('div')
+    opt.classList.add('opt')
+    const optColumn = document.createElement('div')
+    optColumn.classList.add('column')
+    const delBtn = document.createElement('div')
+    delBtn.classList.add('delete')
+    delBtn.innerHTML = 'Delete'
+    const editBtn = document.createElement('div')
+    editBtn.classList.add('edit')
+    editBtn.innerHTML = 'Edit'
+    optColumn.appendChild(delBtn)
+    optColumn.appendChild(editBtn)
+    opt.appendChild(optColumn)
+    optionsDiv.appendChild(toggle)
+    optionsDiv.appendChild(opt)
+    box.appendChild(optionsDiv)
+    commentFunc()
+}
 function generateComment() {
     const box = document.createElement('div')
     box.classList.add('box')
@@ -47,6 +130,8 @@ function generateComment() {
     column.classList.add('column')
     cmntVal = document.createElement('p');
     cmntVal.innerHTML =  `${comment.value}`;
+    console.log(comment.value);
+    cmntVal.id = `comment_${comment.value}`;
     cmntVal.classList.add('comment_val')
     const emailVal = document.createElement('p');
     emailVal.innerHTML =  `${email.value}`;
@@ -93,14 +178,23 @@ function generateComment() {
     optionsDiv.appendChild(opt)
     box.appendChild(optionsDiv)
     commentFunc()
+    // Save the comment to local storage
+    const comments = JSON.parse(localStorage.getItem('comments')) || [];
+    comments.push({
+    user: nameVal.innerHTML,
+    email: emailVal.innerHTML,
+    pic: img.src,
+    comment: cmntVal.innerHTML,
+    date: dateData.innerHTML,
+    id: cmntVal.id
+    });
+    localStorage.setItem('comments', JSON.stringify(comments));
 }
-
 function editFunc(editPrm) {
     editPrm.parentElement.parentElement.classList.remove('active')
     let cmnt = editPrm.parentElement.parentElement.parentElement.parentElement.children[1].children[2];
     let column = editPrm.parentElement.parentElement.parentElement.parentElement.children[1];
     let date = editPrm.parentElement.parentElement.parentElement.parentElement.children[1].children[3];
-    console.log(column);
     const realDate = new Date();
     cmnt.classList.add('none')
     let txt = document.createElement('textarea');
@@ -112,19 +206,38 @@ function editFunc(editPrm) {
     save.innerHTML = 'save'
     column.appendChild(save)
     save.onclick = () => {
-        cmnt.innerHTML = `${txt.value}`
         cmnt.classList.remove('none')
         txt.remove();
         save.remove();
         const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
         date.innerHTML = `${date.innerHTML} (Edited At ${realDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}. ${month[realDate.getMonth()]} ${realDate.getUTCDate()}, ${realDate.getFullYear()})`
+        const comments = JSON.parse(localStorage.getItem('comments')) || [];
+        const commentObj = comments.find(c => c.comment === cmnt.innerHTML);
+        console.log(commentObj);
+        const index = comments.indexOf(commentObj);
+        commentObj.comment = txt.value;
+        commentObj.date = date.innerHTML;
+        localStorage.setItem('comments', JSON.stringify(comments));
+        cmnt.innerHTML = `${txt.value}`
     }
+}
+function deleteComment(comment) {
+    // Find the comment element that contains the comment value
+    const commentElement = document.getElementById(`comment_${comment}`)
+    commentElement.parentElement.parentElement.remove();
+    // Remove the comment from local storage
+    const comments = JSON.parse(localStorage.getItem('comments')) || [];
+    const commentObj = comments.find(c => c.comment === comment);
+    console.log(commentObj);
+    const index = comments.indexOf(commentObj);
+    console.log(index);
+    comments.splice(index, 1);
+    localStorage.setItem('comments', JSON.stringify(comments));
 }
 
 function commentFunc() {
     const optToggle = document.querySelectorAll('.options .toggle')
     const options = document.querySelectorAll('.options .opt')
-    console.log(optToggle);
     optToggle.forEach(optToggle => {
         optToggle.onclick = () => {
             optToggle.nextElementSibling.classList.toggle('active')
@@ -134,8 +247,9 @@ function commentFunc() {
     const edit = document.querySelectorAll('.edit')
     clear.forEach(clear => {
         clear.addEventListener('click', (e) => {
-            const parent = clear.parentElement.parentElement.parentElement.parentElement;
-            parent.remove();
+            const clickedCommentBox = clear.parentElement.parentElement.parentElement.parentElement.querySelector('.comment_val').innerHTML;
+            // Delete the comment
+            deleteComment(clickedCommentBox);
         })
     });
 
@@ -145,8 +259,22 @@ function commentFunc() {
         }
     });
 }
-
-
+window.onload = () => {
+    if(localStorage.getItem("comments")) {
+        
+        const localCmnt = JSON.parse(localStorage.getItem("comments"));
+        if(localCmnt.length > 0 ) {
+        localCmnt.forEach(cmnt => {
+            let user = cmnt.user;
+            let email = cmnt.email;
+            let pic = cmnt.pic;
+            let comment = cmnt.comment;
+            let date = cmnt.date;
+            cmntBuilder(user,email,pic,comment,date);
+        });
+    }
+}
+}
 const submit = document.getElementById('submit')
 const form = document.querySelector('form')
 const user = document.getElementById('name')
@@ -156,7 +284,6 @@ const picture = document.getElementById('pic')
 const cmntSec = document.querySelector('.comments')
 let comment_val;
 submit.addEventListener('click', (e) => {
-    console.log(picture.value);
     e.preventDefault();
     if(user.value == '' || email.value == '' || comment.value == ''){
         alert(`Eneter The Values First`)
@@ -164,8 +291,33 @@ submit.addEventListener('click', (e) => {
         generateComment();
     }
 })
-
 document.querySelector('.bars').addEventListener('click', (e) => {
     document.querySelector('.bars').classList.toggle('open');
     document.querySelector('nav ul').classList.toggle('active')
+})
+function stoppedScrolling() {
+    let last_y = window.scrollY;
+    setTimeout(() => {
+        let new_y = window.scrollY;
+        if(last_y == new_y) {
+            document.querySelector('.nav-bar .wrapper').style.position = 'relative';
+        }else {
+            document.querySelector('.nav-bar .wrapper').style.position = 'fixed';
+        }
+    }, 1000);
+}
+window.onscroll = () => {
+    // stoppedScrolling();
+
+    if(window.scrollY > 300) {
+        toTop.classList.add('active')
+    }else {
+        toTop.classList.remove('active')
+    }
+}
+const toTop = document.querySelector('.top');
+toTop.addEventListener('click', () => {
+    document.querySelector('.nav-bar').scrollIntoView({
+        behavior: 'smooth'
+    });
 })
